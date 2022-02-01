321ed2c8b3a8591dff74452bfea803c59131f183
akashdafane_btc <akash.dafane@tntra.io>
2022-02-01 14:38:08 +0530

    check eslint

--- src/infrastructure/components/table/tableRow.tsx
+++ src/infrastructure/components/table/tableRow.tsx
@@ -16,7 +16,7 @@ const CustomTableRow:React.FC<CustomTableRowProps> = ({
   hover,
   selected,
 }) => {
-  console.log('check eslint');
+  console.log('check eslint   ');
   return (
     <TableRow 
     classes={classes} 
